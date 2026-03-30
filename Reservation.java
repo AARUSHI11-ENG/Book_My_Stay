41d960980682a4aa149c332baa2dc52d1e206dd8
AARUSHI11-ENG <aarushijhawar75@email.com>
2026-03-30 15:53:47 +0530

    Use Case 12 Persistence demo

--- Reservation.java
+++ Reservation.java
@@ -1,13 +1,38 @@
-public class Reservation {
+import java.io.Serializable;
+import java.util.UUID;
 
+public class Reservation implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String reservationId; // Unique reservation ID
     private String guestName;
     private String roomType;
     private int nights;
 
+    // Constructor generates a unique reservation ID automatically
     public Reservation(String guestName, String roomType, int nights) {
+        this.reservationId = generateReservationId();
         this.guestName = guestName;
         this.roomType = roomType;
         this.nights = nights;
+    }
+
+    // Alternative constructor to set a custom reservation ID
+    public Reservation(String reservationId, String guestName, String roomType, int nights) {
+        this.reservationId = reservationId;
+        this.guestName = guestName;
+        this.roomType = roomType;
+        this.nights = nights;
+    }
+
+    private String generateReservationId() {
+        // Generates a unique ID like "RES-xxxx-xxxx"
+        return "RES-" + UUID.randomUUID().toString().substring(0, 8).toUpperCase();
+    }
+
+    // Getters
+    public String getReservationId() {
+        return reservationId;
     }
 
     public String getGuestName() {
@@ -25,7 +50,8 @@ public class Reservation {
     @Override
     public String toString() {
         return "Reservation{" +
-                "guestName='" + guestName + '\'' +
+                "reservationId='" + reservationId + '\'' +
+                ", guestName='" + guestName + '\'' +
                 ", roomType='" + roomType + '\'' +
                 ", nights=" + nights +
                 '}';
